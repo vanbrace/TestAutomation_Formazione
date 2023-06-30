27dbfa88e6a275354b7a776179bab821bf35af4d
NotRoob <137877978+NotRoob@users.noreply.github.com>
2023-06-30 15:17:08 +0200

    Capitolo 6
    Esercizio b del capitolo 6

---
+++ Capitolo 6/EredIncaRius/src/Impiegato.java
@@ -0,0 +1,22 @@
+public class Impiegato extends Persona { //Ereditarietà
+    private int matricola;
+
+    public void setDati(String nome, int matricola) {
+        setNome(nome); //Riuso ed ereditarietà
+        setMatricola(matricola); //Riuso
+
+    }
+
+    public void setMatricola(int matricola) {
+        this.matricola = matricola; //Incapsulamento
+    }
+
+    public int getMatricola() {
+        return matricola; //incapsulamento
+    }
+
+    public String dammiDettagli() {
+    //Riuso, incapsulamento ed ereditarietà
+        return getNome() + ", matricola: " + getMatricola();
+    }
+}
---
+++ Capitolo 6/EredIncaRius/src/Persona.java
@@ -0,0 +1,9 @@
+public class Persona {
+    private String nome;
+    public void setNome (String nome) {
+        this.nome = nome;
+    }
+    public String getNome () {
+        return nome;
+    }
+}

7abb26d03cc8cd2b44feb113a5f4013d715ad0e8
NotRoob <francesco.ciliento@alten.it>
2023-07-04 14:01:20 +0200

    Esercizi 30/06
    Esercizi extra del 30/06

--- Operazioni/src/Operazioni.java
+++ Operazioni/src/Operazioni.java
@@ -63,12 +63,4 @@ public class Operazioni {
         System.out.println("Divisione: " + risultato);
     }
 
-    public static void main(String[] args) {
-        Operazioni operazioni = new Operazioni();
-
-        operazioni.sommaVoid(5, 7);
-        operazioni.sottrazioneVoid(15, 8);
-        operazioni.prodottoVoid(3, 4);
-        operazioni.divisioneVoid(20, 5);
-    }
 }
--- Potenza/src/Potenza.java
+++ Potenza/src/Potenza.java
@@ -14,14 +14,4 @@ public class Potenza {
         base = nuovaBase;
     }
 
-    public static void main(String[] args) {
-        Potenza potenza = new Potenza(2);
-        potenza.pow();
-
-        potenza.cambioBase(3);
-        potenza.pow();
-
-        potenza.cambioBase(4);
-        potenza.pow();
-    }
 }
